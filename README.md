bd8068603fad7a38966b646c46fc03c202ca4130
StevenBloes <steven.bloes@mail.com>
2025-01-08 21:52:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Limousin Rabaut web application
+This project is made specifically made for Limousin Rabaut, a cow farm with an own shop.
+To follow up orders and keep stock of products they were in need of a simple app solution.
+This is a follow up and improvement on their first app written in Java.
---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Limousin - HOME</title>
+    <link rel="icon" href="/img/favicon.svg">
+    <link rel="stylesheet" href="/stylesheets/header.css"
+  </head>
+  <body>
+<?php
+  include 'header.html';
+?>
+    <h1>Natalja is een superjuf !!!</h1>
+    <h2>Kusjes Steven &#128536;</h2>
+  </body>
+</html>
+
