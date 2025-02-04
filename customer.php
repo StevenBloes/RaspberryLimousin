660e1bac0ac85d59a8e547db56f27b849aaf8153
StevenBloes <steven.bloes@mail.com>
2025-02-04 19:31:35 +0100

    Update customer.php
    added full table

--- customer.php
+++ customer.php
@@ -9,16 +9,16 @@
   <body>
 <?php
   include "header.html";
-  echo "<table><tr><th>naam</th><tr>";
+  echo "<table><tr><th>Naam</th><th>Adres</th><th>Postcode</th><th>Plaats</th><th>Tel./GSM</th><th>Mail</th><th>Opmerkingen</th><tr>";
   $host = "192.168.129.9:3306";
   $user = "visitor";
   $pass = "visitor";
   $database = "limousin_db";
   $mysqli = mysqli_connect($host, $user, $pass, $database);
-  $query = "SELECT name FROM customers;";
+  $query = "SELECT name, address, postal, city, phone, mail, remarks FROM customers;";
   $result = mysqli_query($mysqli, $query);
   while ($row = mysqli_fetch_assoc($result)){
-    echo "<tr><td>".$row["name"]."</td></tr>";
+    echo "<tr><td>".$row["name"]."</td><td>".$row["address"]."</td><td>".$row["postal"]."</td><td>".$row["city"]."</td><td>".$row["phone"]."</td><td>".$row["mail"]."</td><td>".$row["remarks"]."</td></tr>";
   }
   $mysqli_close($mysqli);
   echo "</table>";
